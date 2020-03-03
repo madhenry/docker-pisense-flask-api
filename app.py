346bdc2c81d5217780c8ba4c2f3fc4ff0152a1c5
Henry Kehlmann <henry@blooob.me>
2020-03-03 17:15:28 +0200

    Adds /temp and /message/Text endpoints for controlling Sense Hat

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from sense_hat import SenseHat
 from flask import json
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
-def index ():
-
-
+def index():
     try:
         sense = SenseHat()
         c = sense.get_temperature()
@@ -21,6 +20,44 @@ def index ():
 
         response = app.response_class(
             response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
+    
+    except OSError as ose:
+        print("There was an OSError:  {}".format(ose))
+    
+    else:
+        return response
+
+@app.route('/temp')
+def temp():
+    try:
+        sense = SenseHat()
+        temp = sense.get_temperature()
+        cpu_temp = subprocess.check_output("vcgencmd measure_temp", shell=True)
+        temp_calibrated = temp - ((cpu_temp - temp)/5.466)
+
+        response = app.response_class(
+            response=json.dumps(temp_calibrated),
+            status=200,
+            mimetype='application/json'
+        )
+    
+    except OSError as ose:
+        print("There was an OSError:  {}".format(ose))
+    
+    else:
+        return response
+
+@app.route('/message/<string:message>')
+def showMessage(message):
+    try:
+        sense = SenseHat()
+        sense.show_message(message)
+
+        response = app.response_class(
+            response=json.dumps(True),
             status=200,
             mimetype='application/json'
         )
